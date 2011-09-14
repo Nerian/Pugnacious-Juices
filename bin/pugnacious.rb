ef4cf907423aa4c38c695531890a18e51b184737
Nerian <siotopo@gmail.com>
2011-09-14 14:31:45 +0200

    refactor

--- bin/pugnacious.rb
+++ bin/pugnacious.rb
@@ -7,24 +7,25 @@ Ray.game "Pugnacious Juices", :size => [800, 600] do
   register { add_hook :quit, method(:exit!) }    
 
   scene :fight do                                                          
-    @player1 = Player.new(color: Ray::Color.blue)
-    @player2 = Player.new(color: Ray::Color.red, position: [300, 300], control_keys: [:w, :d, :s, :a])
+    @player1 = Player.new()
+    
+    @player2 = Player.new(
+      color: Ray::Color.red, 
+      position: [300, 300], 
+      control_keys: [:w, :d, :s, :a])   
+      
     @players = [@player1, @player2]  
     
     always do      
       @players.each do |player| 
         player.control_keys.each do |direction|
-          if holding? direction
-            player.move direction
-          end
+          if holding? direction then player.move direction end          
         end        
       end                   
     end
     
-    render do |win| 
-      @players.each do |player|
-        win.draw player.pointer
-      end     
+    render do |win|    
+      @players.each { |player| win.draw player.pointer }      
     end
   end     
 
