e4afdc4ccc0ade1d98ca88eae60e4d3b13dca0f5
Nerian <siotopo@gmail.com>
2011-09-14 15:02:21 +0200

    refactor

--- lib/pugnacious/fight_scene.rb
+++ lib/pugnacious/fight_scene.rb
@@ -1,38 +1,40 @@
-class FightScene < Ray::Scene
-  scene_name :fight_scene
+module Pugnacious
+  class FightScene < Ray::Scene
+    scene_name :fight_scene
 
-  def setup
-    @player1 = Player.new()
-    
-    @player2 = Player.new(
+    def setup
+      @player1 = Player.new()
+
+      @player2 = Player.new(
       color: Ray::Color.red, 
       position: [300, 300], 
       control_keys: [:w, :d, :s, :a])   
-      
-    @players = [@player1, @player2]
-  end
 
-  def register
-    always do      
-      @players.each do |player| 
-        player.control_keys.each do |direction|
-          if holding? direction then player.move direction end          
-        end        
+      @players = [@player1, @player2]
+    end
+
+    def register
+      always do      
+        @players.each do |player| 
+          player.control_keys.each do |direction|
+            if holding? direction then player.move direction end                  
+          end
+        end
       end
     end
-  end
 
-  def render(win)
-    @players.each { |player| win.draw player.pointer } 
-  end
-
-  def clean_up
-    @players.each do |player|
-      player = nil
+    def render(win)
+      @players.each { |player| win.draw player.pointer } 
     end
-    
-    @players = nil
-    
-    Ray::ImageSet.clear
+
+    def clean_up
+      @players.each do |player|
+        player = nil
+      end
+
+      @players = nil
+
+      Ray::ImageSet.clear
+    end
   end
 end
