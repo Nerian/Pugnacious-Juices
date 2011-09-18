5fa29914fb50dc73b1538b4bbbdfc8e9aed36e11
Nerian <siotopo@gmail.com>
2011-09-18 16:58:23 +0200

    refactor

--- lib/pugnacious/molecule.rb
+++ lib/pugnacious/molecule.rb
@@ -2,9 +2,7 @@ module Pugnacious
   class Molecule 
     include Pugnacious::Movable
     
-    attr_accessor :player, :life, :body, :molecules, :pointer, :position, :game_map        
-    
-    SPEED = 1
+    attr_accessor :player, :life, :body, :molecules, :pointer, :position, :game_map, :speed
         
     def initialize(options = {})
       @player = options[:player]
@@ -15,8 +13,8 @@ module Pugnacious
       @body = Ray::Polygon.rectangle([0, 0, MOLECULE_SIZE, MOLECULE_SIZE], @player.color)
       @body.pos = [@position[0] * MOLECULE_SIZE, @position[1] * MOLECULE_SIZE] || [0, 0]      
       @molecules = options[:molecules]
-      @pointer = @player.pointer
-      
+      @pointer = @player.pointer                                                           
+      @speed = 1      
     end                                                                                         
   end
 end
--- lib/pugnacious/movable.rb
+++ lib/pugnacious/movable.rb
@@ -1,6 +1,6 @@
 module Pugnacious
   module Movable 
-    
+
     DIRECTIONS = {:north => [0, -1] , 
       :north_east => [1, -1], 
       :east => [1, 0], 
@@ -9,28 +9,41 @@ module Pugnacious
       :south_west => [-1, 1], 
       :west => [-1, 0], 
       :north_west => [-1, -1]}
-            
+
      def move            
        try_to_go(where_is_the_pointer?) unless where_is_the_pointer? == :here 
      end
 
      def try_to_go(direction)                                                        
        if can_i_move_there?(direction)
-         move_there!(direction)           
+         move_there(direction)           
        end              
      end    
 
      def can_i_move_there?(direction)      
        xw, yw = DIRECTIONS[direction]       
 
-       x = xw + @position[0]      
-       y = yw + @position[1]                  
+       x = xw + (@position[0] * @speed)
+       y = yw + (@position[1] * @speed)
 
        if @game_map[x][y] == :empty
          return true
        else         
          return false
        end
+     end
+
+     def move_there(direction) 
+       xw, yw =  DIRECTIONS[direction]    
+
+       x = xw + (@position[0] * @speed)
+       y = yw + (@position[1] * @speed)
+
+       @game_map[@position[0]][@position[1]] = :empty
+       @game_map[x][y] = self
+
+       body.pos = [x * MOLECULE_SIZE, y * MOLECULE_SIZE]
+       @position = [x, y]      
      end
 
     def where_is_the_pointer?
@@ -43,24 +56,6 @@ module Pugnacious
        if pointer_is_down_left then return :south_west end
        if pointer_is_up_left then return :north_west end
        if here then return :here end
-     end
-
-     #####################
-     # Move the molecule
-     #####################            
-
-     # Move it
-     def move_there!(direction) 
-       xw, yw =  DIRECTIONS[direction]    
-
-       x = (xw + @position[0])
-       y = (yw + @position[1])
-
-       @game_map[@position[0]][@position[1]] = :empty
-       @game_map[x][y] = self
-
-       body.pos = [x*MOLECULE_SIZE, y*MOLECULE_SIZE]
-       @position = [x, y]      
      end
 
      def pointer_is_down          
